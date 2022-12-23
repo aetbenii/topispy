a04fdb695ea196de1a52a2df6cc82163b896869a
beni <benny.fcb89@gmail.com>
2022-12-23 14:17:04 +0100

    benjamin - changes

---
+++ src/main/java/Artist.java
@@ -0,0 +1,25 @@
+public class Artist {
+
+    private int artistid;
+    private String artist_name;
+
+    public Artist(){
+
+    }
+
+    public int getArtistid() {
+        return artistid;
+    }
+
+    public void setArtistid(int artistid) {
+        this.artistid = artistid;
+    }
+
+    public String getArtist_name() {
+        return artist_name;
+    }
+
+    public void setArtist_name(String artist_name) {
+        this.artist_name = artist_name;
+    }
+}
--- src/main/java/Song.java
+++ src/main/java/Song.java
@@ -1,9 +1,33 @@
 public class Song {
 
-    private int Songid;
-    private int a;
-
+    private int songid;
+    private String song_name;
+    private String album;
     public Song(){
 
+    }
+
+    public int getSongid() {
+        return songid;
+    }
+
+    public void setSongid(int songid) {
+        this.songid = songid;
+    }
+
+    public String getSong_name() {
+        return song_name;
+    }
+
+    public void setSong_name(String song_name) {
+        this.song_name = song_name;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
     }
 }
